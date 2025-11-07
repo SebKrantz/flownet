38d06d6abedb1f4ce53aadad4ad094f4c86c7b7b
SebKrantz <sebastian.krantz@graduateinstitute.ch>
2025-11-07 10:35:20 -0500

    Clean example a bit.

--- misc/example.R
+++ misc/example.R
@@ -2,8 +2,8 @@ library(mmflowr)
 library(sf)
 
 # GCC Scenario
-od_matrix <- process_od_matrix(od_matrix_directory, cargo_type, period)
-base_network <- st_read("data/network/base_network.shp")
+od_matrix <- process_od_matrix("data/OD_Matrix", "Container", "2019")
+# base_network <- st_read("data/network/base_network.shp")
 network_gc_cnt <- st_read("data/network/base_network_with_gc_containerized")
 zone_nodes <- st_read("data/zone_nodes/network_nodes.shp")
 
@@ -15,17 +15,6 @@ graph_df <- network_gc_cnt |>
   create_undirected_graph()
 
 nodes_df <- nodes_from_graph(graph_df)
-
-graph <- makegraph(graph_df |> fselect(from, to, cost), directed = FALSE) # |>
-  # cpp_contract() # Could use cpp_simplify() to simplify
-nodes <- graph$dict$ref
-dmat <- get_distance_matrix(graph, from = nodes, to = nodes, algorithm = "mch")
-# dmat <- dist_mat_from_df(graph_df)
-dimnames(dmat) <- NULL
-
-get_multi_paths(graph, nodes[1:3], nodes[1:3])
-
-if(!identical(dim(dmat), rep(nrow(nodes_df), 2))) stop("Nodes and Distance Matrix Mismatch")
 
 # Mapping zones to nodes
 nearest_nodes <- st_nearest_feature(zone_nodes, st_as_sf(nodes_df, coords = c("X", "Y"), crs = 4326))
@@ -39,25 +28,23 @@ od_matrix_long <- data.frame(from = rep.int(nearest_nodes, ncol(od_matrix)),
                              flow = vec(od_matrix)) |>
                   fsubset(is.finite(flow) & flow > 0)
 
-
-# Experiment with dodgr
-system.time(dodgr::dodgr_paths(graph_df |> fselect(from, to, dist = cost),
-                   from = seq_row(nodes_df), to = seq_row(nodes_df), vertices = FALSE))
-res <- dodgr::dodgr_paths(graph_df |> fselect(from, to, d = cost),
-                          from = ks, to = ks, vertices = FALSE)
-
-
-# Experiment with igraph: best so far
+# Create Graph igraph: best so far
 library(igraph)
 g <- graph_from_data_frame(graph_df |> fselect(from, to), directed = FALSE)
 g <- delete_vertex_attr(g, "name")
 igraph_options(return.vs.es = FALSE) # sparsematrices = TRUE
-# Distance Matrix
-D <- distances(g, mode = "out", weights = graph_df$cost)
-# Shortest Paths
-shortest_paths(g, from = i, to = ks, weights = graph_df$cost, output = "epath")$epath
 
-cost <- graph_df$cost
+# Distance Matrix
+dmat <- distances(g, mode = "out", weights = graph_df$cost)
+if(!identical(dim(dmat), rep(nrow(nodes_df), 2))) stop("Nodes and Distance Matrix Mismatch")
+
+# Test Shortest Paths
+shortest_paths(g, from = 1, to = 1:100, weights = graph_df$cost, output = "epath")$epath
+
+# Some objects
+beta_PSL <- -0.1 # Path-Sized Logit parameter
+cost <- graph_df$cost # Edge cost
+# Edge incidence across selected routes
 delta_ks <- integer(nrow(graph_df))
 # Final flows vector
 final_flows <- numeric(nrow(graph_df))
@@ -119,47 +106,3 @@ for (i in seq_row(od_matrix_long)) {
 })
 detach(od_matrix_long)
 
-# US Freight Analysis Framework 5.0 Model Network Database
-st_layers(dsn = "/Users/sebastiankrantz/Downloads/Networks/Geodatabase Format/FAF5Network.gdb")
-nodes <- st_read(dsn = "/Users/sebastiankrantz/Downloads/Networks/Geodatabase Format/FAF5Network.gdb", layer = "FAF5_Nodes")
-edges <- st_read(dsn = "/Users/sebastiankrantz/Downloads/Networks/Geodatabase Format/FAF5Network.gdb", layer = "FAF5_Links")
-
-
-#Choose number of cores used by cppRouting
-RcppParallel::setThreadOptions(numThreads = 1)
-
-if(requireNamespace("igraph",quietly = TRUE)){
-
-  #Generate fully connected graph
-  gf<- igraph::make_full_graph(400)
-  igraph::V(gf)$names<-1:400
-
-  #Convert to data frame and add random weights
-  df<-igraph::as_long_data_frame(gf)
-  df$dist<-sample(1:100,nrow(df),replace = TRUE)
-
-  #Construct cppRouting graph
-  graph<-makegraph(df[,c(1,2,5)],directed = FALSE)
-
-  #Pick up random origin and destination node
-  origin<-sample(1:400,1)
-  destination<-sample(1:400,1)
-
-  #Compute distance from origin to all nodes
-  or_to_all<-get_distance_matrix(graph,from=origin,to=1:400)
-
-  #Compute distance from all nodes to destination
-  all_to_dest<-get_distance_matrix(graph,from=1:400,to=destination,)
-
-  #Get all shortest paths from origin to destination, passing by each node of the graph
-  total_paths<-rowSums(cbind(t(or_to_all),all_to_dest))
-
-  #Compute shortest path between origin and destination
-  distance<-get_distance_pair(graph,from=origin,to=destination)
-
-  #Compute detour with an additional cost of 3
-  det<-get_detour(graph,from=origin,to=destination,extra=3)
-
-  #Check result validity
-  length(unlist(det))
-  length(total_paths[total_paths < distance + 3])
